e0de540d81da1bd86038def78d02b9bd70fb5bb3
VladimirCherednichenko <cherid95@gmail.com>
2025-04-03 18:07:39 +0300

    removed unused timeout const

--- src/main/java/com/marketpay/utils/Constants.java
+++ src/main/java/com/marketpay/utils/Constants.java
@@ -6,7 +6,6 @@ public class Constants {
     public static final int ELEMENT_VISIBILITY_TIMEOUT = 30;
     public static final int ELEMENT_CLICKABLE_TIMEOUT = 30;
     public static final int PAGE_LOAD_TIMEOUT = 30;
-    public static final int AJAX_TIMEOUT = 20;
     public static final int IFRAME_TIMEOUT = 15;
     public static final int SHORT_TIMEOUT = 5;
     public static final int EXTENDED_TIMEOUT = 60;
